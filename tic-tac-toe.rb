4bf29ed319a2e38faf9b825ebf1e511b6f898304
JayJay <timothyjapolinario@gmail.com>
2022-05-04 12:55:06 +0800

    Add game, player, and computer classes

---
+++ tic-tac-toe.rb
@@ -0,0 +1,17 @@
+class Game
+
+    @@row_slots = [[0, 0, 0],[1, 1, 1],[2, 2, 2]]
+
+
+    def initialize(player_1, player_2)
+        @player_1 = player_1
+        @player_2 = player_2
+    end
+
+end
+
+class Player
+end
+
+class Computer
+end
