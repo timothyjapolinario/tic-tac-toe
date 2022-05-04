c8070e7e525b48f2b2b9fbbea7368973b26d3d34
JayJay <timothyjapolinario@gmail.com>
2022-05-04 16:38:41 +0800

    Recreate logic for printing the board

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,41 +1,47 @@
 class Game
 
-    @@row_slots = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
-
+    attr_reader :board
 
     def initialize(player_1_class, player_2_class)
         @player_1_class = player_1_class
         @player_2_class = player_2_class
+        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
     def play()
         player_1 = @player_1_class.new(self)
         player_2 = @player_2_class.new(self)
-        create_board()
+        player_1.select_marker()
     end
     def create_board()
         col_separator, row_separator = "|", "---+---+---"
 
-        @@row_slots.each_with_index do |row, index|
-            row.each_with_index do |slot, index|
-                print " #{slot} #{col_separator if(index != 2)}"
-            end
-            puts "\n"
-            puts row_separator if(index != 2)
+        @board.each do |slot|
+            print " #{slot} #{(slot % 3)==0? "\n": col_separator}"
+            print "#{row_separator+"\n" if((slot % 3)==0 && slot < 7)}"        
         end
     end
 
+    def add_marker
+        create_board()
+    end
 end
 
 class Player
+    attr_reader :markers
     def initialize(game)
         @game = game
+        @markers = []
+    end
+
+    def select_marker()
+        @game.create_board()
     end
 end
 
 class HumanPlayer < Player
 end
 
-class ComputerPlayer
+class ComputerPlayer < Player
 end
 
 game1 = Game.new(HumanPlayer, HumanPlayer)
