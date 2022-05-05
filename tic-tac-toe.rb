dd0c86e7f1ee14dae1fe9b01b8a55aead7a0d70c
JayJay <timothyjapolinario@gmail.com>
2022-05-05 16:57:08 +0800

    Add checking of winners functionality

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,7 @@
-class Game
+require 'pry-byebug'
 
+class Game
+    @@lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     attr_reader :board
     @@current_player = nil
     def initialize(player_1_class, player_2_class)
@@ -13,13 +15,23 @@ class Game
     end
 
     def play()
-        loop do
+        total_moves = 9
+        while (total_moves != 0)
             create_board()
             puts "Player #{@@current_player.id} select from the available slots"
             selected_marker = gets.chomp.to_i
             @@current_player.select_marker(selected_marker)
+
+            has_winner = check_winner?(@@current_player.markers)
+            if(has_winner)
+                return "Player #{@@current_player.id} winner!"
+                create_board()
+            end
+
             switch_player()
+            total_moves -= 1
         end
+        puts "No player has won!"
     end
 
     def create_board()
@@ -39,6 +51,15 @@ class Game
 
     def switch_player()
         @@current_player = @@current_player == @player_1? @player_2 : @player_1
+    end
+
+    def check_winner?(markers)
+        @@lines.reduce(false) do |result, line|
+            if((line - markers).empty? && (markers.length > 2))
+                result = true
+                return result
+            end
+        end
     end
 end
 
