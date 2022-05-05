862e2134d688124a638da3837ea4002d188937a2
JayJay <timothyjapolinario@gmail.com>
2022-05-05 12:10:24 +0800

    Add selecting markers  for 2 players

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,47 +1,61 @@
 class Game
 
     attr_reader :board
-
+    @@current_player = nil
     def initialize(player_1_class, player_2_class)
         @player_1_class = player_1_class
         @player_2_class = player_2_class
-        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        @player_1 = @player_1_class.new(self, 1)
+        @player_2 = @player_2_class.new(self, 2)
+        @board = {1 =>1, 2 => 2, 3 => 3, 4 => 4, 5 =>  5, 6 => 6, 7 => 7, 8 => 8, 9 => 9}
+        @@current_player = @player_1
+
     end
 
     def play()
-        player_1 = @player_1_class.new(self)
-        player_2 = @player_2_class.new(self)
-        player_1.select_marker()
+        loop do
+            create_board()
+            puts "Player #{@@current_player.id} select from the available slots"
+            selected_marker = gets.chomp.to_i
+            @@current_player.select_marker(selected_marker)
+            switch_player()
+        end
     end
 
     def create_board()
         col_separator, row_separator = "|", "---+---+---"
-
-        @board.each do |slot|
-            print " #{slot} #{(slot % 3)==0? "\n": col_separator}"
-            print "#{row_separator+"\n" if((slot % 3)==0 && slot < 7)}"        
+        @board.each do |key, slot|
+            print " #{slot} #{(key % 3)==0? "\n": col_separator}"
+            print "#{row_separator+"\n" if((key % 3)==0 && key < 7)}"        
         end
     end
     
-    def add_marker(slot)
-        @board[slot - 1] = "X"
-        create_board()
+    def place_player_marker(markers)
+        markers.each do |slot|
+            @board[slot] = @@current_player == @player_1? "X" : "O"
+        end
+        
+    end
+
+    def switch_player()
+        @@current_player = @@current_player == @player_1? @player_2 : @player_1
     end
 end
 
 class Player
     attr_reader :markers
-    def initialize(game)
+    attr_reader :id
+    def initialize(game, id)
         @game = game
         @markers = []
+        @id = id
     end
 
 
-    def select_marker()
-        temp_marker = 5
-        if (@game.board.include?(temp_marker))
-            @markers.push(temp_marker)
-            @game.add_marker(temp_marker)
+    def select_marker(selected_marker)
+        if (@game.board[selected_marker] == selected_marker)
+            @markers.push(selected_marker)
+            @game.place_player_marker(markers)
         end
     end
 end
